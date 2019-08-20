4d6e505ce71a4906769e2a93bd987c68aaebf80b
hg_jeon <jeonhgfm101@gmail.com>
2019-08-20 23:22:46 +0900

    미국주식이 추가되지 않은 오류 해결

--- data/views.py
+++ data/views.py
@@ -46,20 +46,21 @@ def index(request):
     if not CoData_instance:
         for i in CoName_instance:
             CoData.objects.create(name=i, data=get_data_json("%s" % i.name))
-        staples = ['GIS', 'HRL', 'K', 'KHC', 'KO', 'MCD', 'MDLZ', 'MO', 'PEP', 'SBUX', 'STZ', 'WMT']
-        giants = ['AAPL', 'AMZN', 'FB', 'GOOG', 'NFLX', 'MSFT']
-        for item in staples:
-            if item not in CoName_instance:
-                CoName.objects.create(name = item)
-            if item not in CoData_instance:
-                CoData.objects.create(name = CoName.objects.get(name=item), data = adjclose_list(item)['Adj Close'])
-        for item in giants:
-            if item not in CoName_instance:
-                CoName.objects.create(name = item)
-            if item not in CoData_instance:
-                CoData.objects.create(name = CoName.objects.get(name=item), data = adjclose_list(item)['Adj Close'])
-    return render(request, 'data/index.html')
 
+    staples = ['GIS', 'HRL', 'K', 'KHC', 'KO', 'MCD', 'MDLZ', 'MO', 'PEP', 'SBUX', 'STZ', 'WMT']
+    giants = ['AAPL', 'AMZN', 'FB', 'GOOG', 'NFLX', 'MSFT']
+
+    for item in staples:
+        if item not in CoName_instance:
+            CoName.objects.create(name = item)
+        if item not in CoData_instance:
+            CoData.objects.create(name = CoName.objects.get(name=item), data = adjclose_list(item)['Adj Close'])
+    for item in giants:
+        if item not in CoName_instance:
+            CoName.objects.create(name = item)
+        if item not in CoData_instance:
+            CoData.objects.create(name = CoName.objects.get(name=item), data = adjclose_list(item)['Adj Close'])
+    return render(request, 'data/index.html')
 
 def ready(request):
     if request.method == 'POST':
