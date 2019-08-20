43bfb2e4ac5d52cb49da731dc66e2dc0e0d13be3
김지윤 <kimjiyoon1006@hanmail.net>
2019-08-20 15:42:14 +0900

    회원가입 html

--- YoungCha_x2/settings.py
+++ YoungCha_x2/settings.py
@@ -197,9 +197,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/','/static2/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static2',)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static',)
+STATIC_DIRS  = (
+    os.path.join(BASE_DIR, 'static'),
 
+)
 
 
 AUTH_USER_MODEL = "users_management.UserManage"
