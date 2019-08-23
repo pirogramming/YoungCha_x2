5332a41efc357bcb655b9cb0797f90ff00fda8b3
KilJaeeun <rha3122@naver.com>
2019-08-23 21:50:29 +0900

    static 모음

--- YoungCha_x2/settings.py
+++ YoungCha_x2/settings.py
@@ -85,7 +85,10 @@ ALLOWED_HOSTS = [
 
     '127.0.0.1',
     'http://127.0.0.1:8000/admin',
-    'kiljaeeun.pythonanywhere.com'
+    'kiljaeeun.pythonanywhere.com',
+    '*',
+    '52.79.206.182',
+
 
 ]
 
@@ -198,7 +201,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIR= os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
+
 AUTH_USER_MODEL = "users_management.UserManage"
 
 from django.urls import reverse_lazy
--- accounts/urls.py
+++ accounts/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
   #  path('reset/<uidb64>/<token>/', views.MyPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
